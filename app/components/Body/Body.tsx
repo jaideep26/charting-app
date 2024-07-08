27b6c5901931fd06f4c11709342aec646609e485
jaideep26 <87374321+jaideep26@users.noreply.github.com>
2024-07-08 13:21:53 -0400

    Basic App Template with a Card

---
+++ app/components/Body/Body.tsx
@@ -0,0 +1,11 @@
+import GraphCard from "../GraphCard/GraphCard";
+
+export default function Body() {
+    return (
+      <main>
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <GraphCard/>
+        </div>
+      </main>
+    );
+  }
---
+++ app/components/GraphCard/GraphCard.tsx
@@ -0,0 +1,15 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+// TODO - Put a graph in this card
+export default function GraphCard() {
+    return(
+        <>
+            <Card variant='outlined'>
+                <CardContent>
+                    Hello World.
+                </CardContent>
+            </Card>
+        </>
+    )
+};
